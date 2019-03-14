4cc17b86258e2f8883b5aed9ef320e5709a2ff9d
Marc Benigno <mjgbenigno@gmail.com>
2019-03-14 17:39:54 -0500

    VotingBlock changes
    filling in more logic into methods

--- VoterApp/src/VotingBlockchain.java
+++ VoterApp/src/VotingBlockchain.java
@@ -1,26 +1,58 @@
 import Blocks;
 
 public class VotingBlockchain{
-Crypto c;
-Blocks b;
+private Crypto c;
+private Blocks b;
 
-public LinkedList<Blocks> ledger = new LinkedList<Blocks>(); //this is a list that contains Blocks objects
+private static LinkedList<Blocks> ledger = new LinkedList<Blocks>(); //this is a list that contains Blocks objects
 
     public VotingBlockChain(){
         c = new Crypto();
         //initialize chain
         b = new Blocks(); //should return a blocks object containing the genesis block
+        ledger.add(0,b);
     }
 
+    /**
+     * Adds a new block to the ledger LinkedList
+     */
     public void addBlockToChain(Blocks newBlock){
-
+        
     }
 
+    /**
+     * Verify this ledger from beginning to end;
+     */
     public void verifyLedger(){
 
     }
 
+    /**
+     * Verify a specific ledger
+     */
     public void verifyLedger(LinkedList ledger){
 
+    }
+
+    public void verifyBlockInLedger(int blockIndex){
+        Blocks previousBlock,blockToCheck;
+        if (blockIndex == 0) {
+            //check genesis block
+            Blocks genesis = new Blocks();
+            blockToCheck = ledger.get(0);
+
+        } else if (blockIndex == 1){
+            previousBlock = new Blocks();
+            blockToCheck = ledger.get(1);
+        } else {
+            previousBlock = ledger.get(blockIndex - 1);
+            blockToCheck = ledger.get(blockIndex);
+        }
+
+        //TODO
+    }
+
+    public LinkedList getLedger(){
+        return ledger;
     }
 }
